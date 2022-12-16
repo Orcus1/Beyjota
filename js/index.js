7cdb48b43960b78287091b538fe5d4db0684c8b0
Tiziano Huerta <tizianohuerta123@gmail.com>
2022-12-16 20:33:58 -0300

    Cuarta Prueba

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 document.querySelector(".bars__menu").addEventListener("click", animateBars);
+document.querySelector(".navElement3").addEventListener("click", animateBars);
+document.querySelector(".navElement4").addEventListener("click", animateBars);
 
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
 var container__menu = document.querySelector(".menu");
-var oscurecerOff = document.querySelector(".oscurecerOff")
-var navElement1 = document.querySelector(".navElement1")
-var navElement2 = document.querySelector(".navElement2")
-var navElement3 = document.querySelector(".navElement3")
-var navElement4 = document.querySelector(".navElement4")
+var oscurecerOff = document.querySelector(".oscurecerOff");
+var navElement1 = document.querySelector(".navElement1");
+var navElement2 = document.querySelector(".navElement2");
+var navElement3 = document.querySelector(".navElement3");
+var navElement4 = document.querySelector(".navElement4");
 
 function animateBars(){
     line1__bars.classList.toggle("activeline1__bars-menu");
@@ -16,8 +18,8 @@ function animateBars(){
     line3__bars.classList.toggle("activeline3__bars-menu");
     container__menu.classList.toggle("menu__active");
     oscurecerOff.classList.toggle("oscurecer");
-    navElement1.classList.toggle("navElementActive1")
-    navElement2.classList.toggle("navElementActive2")
-    navElement3.classList.toggle("navElementActive3")
-    navElement4.classList.toggle("navElementActive4")
+    navElement1.classList.toggle("navElementActive1");
+    navElement2.classList.toggle("navElementActive2");
+    navElement3.classList.toggle("navElementActive3");
+    navElement4.classList.toggle("navElementActive4");
 }
